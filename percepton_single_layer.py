7fbf71e1fa12be38af9cc3c91d5e347d10128ca1
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-08-14 19:33:23 -0300

    Percepton single layer sum and activation function

--- percepton_single_layer.py
+++ percepton_single_layer.py
@@ -5,4 +5,10 @@ import itertools
 inputs = [1, 7, 5]
 weights = [0.8, 0.1, 0]
 
-print(sum(x*w for x, w in itertools.product(inputs, weights)))
+sumxw = sum(x*w for x, w in zip(inputs, weights))
+
+print(sumxw)
+
+activation_fuction = lambda x: 1 if x > 0 else 0
+
+print(activation_fuction(sumxw))
