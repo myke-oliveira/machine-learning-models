289db2c991fd6aaceeee33179765d36d35f221cf
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-08-14 21:07:42 -0300

    Persepton Single Layer AND Logic compleated

--- percepton_single_layer.py
+++ percepton_single_layer.py
@@ -5,19 +5,19 @@ import numpy as np
 learning_ratio = .1
 
 print('Train set - Logic AND')
-X = [np.array((0, 0)), np.array((0, 1)), np.array((1, 0)), np.array((1, 1))]
+X = np.array(((0, 0), (0, 1), (1, 0), (1, 1)))
 
-Y = [0, 0, 0, 1]
+Y = np.array([0, 0, 0, 1])
 print('X     Y')
 for x, y in zip(X, Y):
 	print(x, y)
 print()
 
-weights = np.array((0, 0))
+weights = np.array((0., 0.))
 print(f'Weights = {weights}')
 
 activation_fuction = lambda x: 1 if x >= 1 else 0
-neural_output = lambda x: x.dot(weights)
+neural_output = lambda x: activation_fuction(x.dot(weights))
 
 # Output from Neural Network
 print(f'Output from Neural Network:')
@@ -28,11 +28,10 @@ print()
 
 # Train Neural Network
 print('Training Neural Network...')
-
-while True:
+while (any(y - neural_output(x) for x, y in zip(X, Y))):
 	for x, y in zip(X, Y):
-		print(x, y)
-		# weights = weights + learning_ratio * x * (y-)
-	break
+		weights += learning_ratio * x * (y - neural_output(x))
 
-
+print('Final Output')
+for x in X:
+	print(x, neural_output(x))
